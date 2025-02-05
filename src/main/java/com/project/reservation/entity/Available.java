f9deea6a90a2addeb7292ed523edfc1376bebb95
yjuni0 <alstn9654@naver.com>
2025-02-05 19:22:38 +0900

    엔티티 수정

--- src/main/java/com/project/reservation/entity/Available.java
+++ src/main/java/com/project/reservation/entity/Available.java
@@ -30,39 +30,14 @@ public class Available {
     @Column(nullable = false)
     private LocalDateTime endTime;
 
+
     @Builder
     public Available(Long id, List<Slot> slots, Department department, LocalDateTime startTime, LocalDateTime endTime) {
         this.id = id;
         this.slots = slots;
         this.department = department;
-        // 기본 startTime과 endTime을 현재 날짜 기준으로 설정
-        LocalDateTime todayStartTime = LocalDateTime.now().toLocalDate().atTime(9, 0);  // 오늘 오전 9시
-        LocalDateTime todayEndTime = LocalDateTime.now().toLocalDate().atTime(18, 0);  // 오늘 오후 6시
-
-        this.startTime = startTime != null ? startTime : todayStartTime;
-        this.endTime = endTime != null ? endTime : todayEndTime;
+        this.startTime = startTime;
+        this.endTime = endTime;
 
     }
-    // 슬롯 자동 생성 로직 (예: 30분 단위로 생성)
-    public void generateSlotsForMonth() {
-        List<Slot> generatedSlots = new ArrayList<>();
-        LocalDateTime slotTime = startTime;
-
-        // startTime 부터 endTime 까지 30분 단위로 Slot 생성
-        while (!slotTime.isAfter(endTime)) {
-            Slot slot = Slot.builder()
-                    .available(this)
-                    .state(true)  // 기본적으로 활성 상태
-                    .startTime(slotTime)
-                    .endTime(slotTime.plusMinutes(30)) // 30분 단위
-                    .build();
-
-            generatedSlots.add(slot);
-            slotTime = slotTime.plusMinutes(30); // 30분씩 증가
-        }
-
-        this.slots = generatedSlots;
-    }
-
-
 }
