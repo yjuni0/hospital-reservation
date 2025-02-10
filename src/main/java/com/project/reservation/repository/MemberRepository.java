c502d0c87909b1e0bb7034cfb129a2626b911c52
yjuni0 <alstn9654@naver.com>
2025-02-10 11:50:26 +0900

    test repository 검색 기능 추가

---
+++ src/main/java/com/project/reservation/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.project.reservation.repository;
+
+import com.project.reservation.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByNickName(String nickName);
+}
